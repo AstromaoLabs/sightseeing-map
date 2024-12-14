0d13777d2a605ac9d357bd28945064d40f70be0a
AnSiChen <anthony3em@gmail.com>
2024-12-14 18:15:21 +0800

    chore: clean file for security

--- backend/sightseeing_map/settings.py
+++ backend/sightseeing_map/settings.py
@@ -29,9 +29,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simpleapi',
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
 ]
 
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000', cast=Csv())
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
