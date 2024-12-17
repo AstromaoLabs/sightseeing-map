309c63675cb3247e7da23718d79daddfd234e1d2
AnSiChen <anthony3em@gmail.com>
2024-12-18 00:47:38 +0800

    Connect project to user app for user authentication api

--- backend/sightseeing_map/urls.py
+++ backend/sightseeing_map/urls.py
@@ -15,8 +15,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+# Swagger and Redoc documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sightseeing Map API",                                     
+        default_version='v1',                                            
+        description="API documentation for the Sightseeing Map project, which provides users with sightseeing locations and user authentication.",
+        terms_of_service="https://example.com/policies/terms/",   # To be updated     
+        contact=openapi.Contact(email="example@example.com"),      # To be updated  
+        license=openapi.License(name="Propietary"),                # To be updated                   
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# URL patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sightseeing/v1/', include('users.urls')), # Include the users app
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), # Keep This Three
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),                 # Lines While drf-yasg
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),                          # Is in used. 
 ]
+
+# Using the following URL pattern:
+# {subdomain}.{domain}/{project-name}/{version}/{resource}/{action}/{id}
