9be24c9e02dc892abcc2b608ae03626462b7b986
AnSiChen <anthony3em@gmail.com>
2024-12-20 12:09:16 +0800

    Implement logout endpoint

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from .views import RegisterAPI, LoginAPI, UserAPI
+from .views import RegisterAPI, LoginAPI, UserAPI, LogoutAPI
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('users/register/', RegisterAPI.as_view(), name='user-register'),
     path('users/login/', LoginAPI.as_view(), name='user-login'),
+    path('users/logout/', LogoutAPI.as_view(), name='user-logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Revise location if needed
     path('users/me/', UserAPI.as_view(), name='user-detail'),
 ]
